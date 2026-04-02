b475bb4dac4bcb97c36f95a4bca336bcaeeb85ee
marcturu <marcturu80@gmail.com>
2026-04-02 18:50:28 +0200

    category structure created + header bugs fixed

--- src/assets/scripts/modules/header.js
+++ src/assets/scripts/modules/header.js
@@ -1,16 +1,27 @@
 export function initHeader() {
+  history.scrollRestoration = 'manual';
+  window.scrollTo(0, 0);
+
   const header = document.querySelector('.header');
   const menuBtn = document.querySelector('.header__menu-btn');
   const mobileNav = document.querySelector('.header__mobile-nav');
   const mobileLink = document.querySelectorAll('.header__mobile-link');
   const logo = document.querySelector('.header__logo');
 
+  const isHome = ['/', '/index.html', 'index'].includes(window.location.pathname);
+
   const getScrollRatio = () => Math.min(window.scrollY / 1000, 1);
+
   const updateHeaderOpacity = () => {
     const isMenuOpen = mobileNav.classList.contains('header__mobile-nav--open')
     if (isMenuOpen) return
-    header.style.setProperty('--header-opacity', getScrollRatio())
+    
+    const scrollRatio = getScrollRatio()
+    header.style.setProperty('--header-opacity', scrollRatio)
+    header.style.setProperty('--border-opacity', isHome ? scrollRatio : 1)
   }
+
+  updateHeaderOpacity();
 
   let ticking = false;
   window.addEventListener('scroll', () => {
@@ -32,6 +43,8 @@ export function initHeader() {
 
     if (isOpen) {
       header.style.setProperty('--header-opacity', 1);
+      header.style.setProperty('--header-fg', 1);
+      header.style.setProperty('--border-opacity', 1);
     } else {
       updateHeaderOpacity();
     }
@@ -40,27 +53,20 @@ export function initHeader() {
   /* Cerrar menú mobile al hacer click en un link */
   mobileLink.forEach((link) => {
     link.addEventListener('click', () => {
-
       mobileNav.classList.remove('header__mobile-nav--open')
+      mobileNav.setAttribute('aria-hidden', true);
       menuBtn.setAttribute('aria-expanded', false);
       menuBtn.querySelector('i').className = 'fas fa-bars';
       updateHeaderOpacity();
-
-      window.scrollTo({
-        top: document.querySelector(target).offsetTop,
-        behavior: 'smooth'
-      })
-      
     })
   })
 
   // Cerrar menú mobile al hacer click en el logo
   logo.addEventListener('click', () => {
     mobileNav.classList.remove('header__mobile-nav--open')
+    mobileNav.setAttribute('aria-hidden', true);
     menuBtn.setAttribute('aria-expanded', false);
     menuBtn.querySelector('i').className = 'fas fa-bars';
     updateHeaderOpacity();
-
   })
-
 }
